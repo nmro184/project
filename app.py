401d6aa221bcc7bd714d35499d1b4c61cd258361
nmro184 <nimrod.balaban@gmail.com>
2024-02-20 23:45:48 +0200

    fix done

--- app.py
+++ app.py
@@ -24,21 +24,15 @@ def create(username):
     new_task(name = request.args['name'] , duedate=request.args['duedate'] , day = day_of_week , username = username )
     return redirect(f"/home/{username}")
 
-@app.route('/week')
-def week():
-    tasks_list = get_tasks()
-    week_instance = Week(tasks_list)
-    return render_template('week.html',week = json.dumps(week_instance.to_dict()))
-
 @app.route('/delete/<username>' , methods = ['POST', 'GET'])
 def delete(username):
     delete_task(request.args['deleteid'])
     return redirect(f"/home/{username}")
 
-@app.route('/done/<task_id>')
-def done(task_id):
+@app.route('/done/<task_id>/<username>')
+def done(task_id , username):
     done_task(task_id)
-    return home()
+    return redirect(f"/home/{username}")
 
 @app.route('/api/task/<task_id>')
 def get_task_api(task_id):
